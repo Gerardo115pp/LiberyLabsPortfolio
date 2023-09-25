b331c4e8c7042aeec7922921005c9204ddca4762
Gerardo <theronin115@gmail.com>
2023-09-24 21:53:01 -0600

    Testing CI/CD changes v1.7

--- Services/Portfolio/portfolio_service.go
+++ Services/Portfolio/portfolio_service.go
@@ -40,7 +40,7 @@ func main() {
 		echo.Echo(echo.RedFG, "Error while creating admin repository")
 		echo.EchoFatal(err)
 	}
-	//this is usless v.2.1.4
+	//this is usless v.2.2
 	project_ideas_repository, err := database.NewProjectIdeasRepository()
 	if err != nil {
 		echo.Echo(echo.RedFG, "Error while creating admin repository")
